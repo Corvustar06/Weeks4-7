a70bfc7c4776aac0a9c2a58133ef55159794c73c
Kyara Schmitz <schmitzk@shernet.sheridancollege.ca>
2026-01-27 11:50:47 -0500

    Did the coding gym exercise

---
+++ Assets/Scripts/PowerSwitch.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PowerSwitch : MonoBehaviour
+{
+    public float speed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newRot = transform.eulerAngles;
+        newRot.z += speed;
+        transform.eulerAngles = newRot;
+    }
+
+    public void StartSpin()
+    {
+        speed = 100;
+    }
+    public void StopSpin()
+    {
+        speed = 0;
+    }
+}
